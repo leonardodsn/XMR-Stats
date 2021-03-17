09858bb3d54ba4c7e640d427794a6cb00eec1fb7
Leonardo <contato@leonardonicolazzi.co>
2021-03-17 20:11:52 -0300

    ReadME Update

--- DaemonRPC/xmrPullChain.sh
+++ DaemonRPC/xmrPullChain.sh
@@ -7,15 +7,9 @@ conf_file='pullChain.conf'  # < Configuration file name
 
 #________________________________________
 #_ Pull from conf
+. ./$conf_file
 
-database='xmrstats'         # < Your PostgreSQL database name
-user='lsa'                  # < Your PostgreSQL user name
-ip='127.0.0.1'              # < monerod (Monero Daemon) IP
-port='18081'                # < monerod (Monero Daemon) port
-json_s=100                  # < size of json in blocks, in doubt leave default
-cpu_t=8                    # < amount of cpu threads
-
-#_ GETS CURRENT DBHEIGHT
+#_ GETS CURRENT POSTGRESQL DBHEIGHT
 dbheight_command="psql -U $user -d $database -c \"SELECT MAX(height) FROM block\""
 dbheight=$(eval $dbheight_command)               # < current db_height
 dbheight=$(echo ${dbheight/"(1 row)"})
@@ -23,10 +17,14 @@ dbheight=$(echo "${dbheight//[^0-9.]/}")
 dbheight=$(expr $dbheight - 1)
 
 #_ GETS CURRENT BLOCKCHAIN HEIGHT
-cheight=2313585             # < current block chain height
+bc_height_url="curl http://127.0.0.1:18081/get_height -H 'Content-Type: application/json'"
+bc_height_url="http://$ip:$port/get_height -H 'Content-Type: application/json'"
+bc_height_command="curl ${bc_height_url}"
+bc_height=$(eval $bc_height_command)
+bc_height=$(echo "$bc_height" | jq -r '.height')
 
 loop_s=$(expr $json_s \* $cpu_t)
-left=$(expr $cheight - $dbheight)
+left=$(expr $bc_height - $dbheight)
 
 if [ $(expr $left % $loop_s) = 0 ]; then
     loops=$(expr $left / $loop_s)
