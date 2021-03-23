0fddc0eadce645fcc7c9054ba62cc0578e83265f
Leonardo <contato@leonardonicolazzi.co>
2021-03-23 12:24:47 -0300

    Block Updater

--- DaemonRPC/blocks/blockUpdater.sh
+++ DaemonRPC/blocks/blockUpdater.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 #Pulls from Daemon every 10 second to update for new blocks
+
+conf_file=$1
+#________________________________________
+#_ Pull from conf
+. ../$conf_file
+
+source ../func.sh
+
+while [ "$update" = "true" ]
+do
+    dbheight=$(db_height)
+    bcheight=$(bc_height)
+    
+    [[ $bcheight > $dbheight ]] && bash ./insertRecord.sh "" $(expr $dbheight + 1) $(expr $dbheight + 1) 1 $conf_file && echo "New block found" || sleep 20
+    
+done
