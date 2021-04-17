d1ade1f0166426e4a9ebca13a80553f69f07d971
Leonardo <contato@leonardonicolazzi.co>
2021-04-16 22:07:14 -0300

    Tx in Gaps finder

--- DaemonRPC/tx/txFindGaps.sh
+++ DaemonRPC/tx/txFindGaps.sh
@@ -28,8 +28,23 @@ do
     dbgap=$(echo ${dbgap/"(1 row)"})
     dbgap=$(echo "${dbgap//[^0-9.]/}")
     dbgap=$(expr $dbgap)
-    
-    echo $dbgap
 
     [[ $dbgap = 0 ]] && gap="false" && echo "no gaps" || ([[ $gap_s < $json_tx_s && $dbgap != 0 ]] && bash ./insertTxRecord.sh $gap $dbgap $(expr $dbgap + 9) 10 $conf_file ) || (bash ./createTxProcesses.sh $dbgap $(expr $dbgap + $(expr $json_s \* $cpu_t ) ) $cpu_t $json_tx_s $conf_file)
 done
+
+
+#_ GET >IN BLOCK< GAPS (lacking transactions in a block)
+
+in_gap="true"
+
+while [ "$in_gap" = "true" ]
+do
+    gap_search="psql -U $user -d $database -c \"SELECT MIN(block_height) FROM (select count(*) as b_count ,block.height as block_height ,(block.num_txes + 1) as b_tx from tx left join block on block.height = tx.height  WHERE block.height < $dbheight AND block.num_txes > 0 group by block.height, block.num_txes) as aa WHERE b_count <> b_tx\""
+    dbgap=$(eval $gap_search)
+    dbgap=$(echo ${dbgap/"(1 row)"})
+    dbgap=$(echo "${dbgap//[^0-9.]/}")
+    dbgap=$(expr $dbgap)
+
+    [[ $dbgap = 0 ]] && gap="false" && echo "no gaps" || ([[ $gap_s < $json_tx_s && $dbgap != 0 ]] && bash ./insertTxRecord.sh $gap $dbgap $(expr $dbgap + 9) 10 $conf_file ) || (bash ./createTxProcesses.sh $dbgap $(expr $dbgap + $(expr $json_s \* $cpu_t ) ) $cpu_t $json_tx_s $conf_file)
+done
+
